58f8be988b15c46184ab49d8a624fb7117609eb2
ali <aliamiryance@gmail.com>
2019-07-01 19:26:49 +0430

    edit managements
    Signed-off-by: ali <aliamiryance@gmail.com>

---
+++ src/service/Search.java
@@ -0,0 +1,25 @@
+package service;
+
+import Model.Document;
+import Model.User;
+
+import java.util.List;
+
+public class Search {
+
+    public static List<User> searchUser(){
+        return null;
+    }
+
+    public static List<Document> searchDocument(){
+        return null;
+    }
+
+    public static List<User> filterUser(){
+        return null;
+    }
+
+    public static List<Document> filterDocument(){
+        return null;
+    }
+}
