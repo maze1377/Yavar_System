b0f73b597f88a52edc734d8af3420b5a18fec919
ali <aliamiryance@gmail.com>
2019-07-01 17:15:52 +0430

    edit managements
    Signed-off-by: ali <aliamiryance@gmail.com>

--- src/Management/ProfileManagement.java
+++ src/Management/ProfileManagement.java
@@ -1,8 +1,13 @@
 package Management;
 
 import DB.Db_Handler;
+import Model.Date;
+import Model.Document;
+import Model.Order;
 import Model.User;
 import setting.Setting;
+
+import java.util.List;
 
 public class ProfileManagement {
 
@@ -11,7 +16,7 @@ public class ProfileManagement {
         Db_Handler.getDatabaseHandler(Setting.Db_Table_name.User).updateUser(user);
     }
 
-    public static void createUser(){
-        //User user = new User()
+    public static User createUser(List<Order> orderList, List<Document> myDocuments, String userName, String password, String firstName, String lastName, String nationalCode, String tel, String email, String field, String university, String certificate, Date enterDate, String creditCardNumber, long credit){
+        return new User(orderList, myDocuments, userName, password, firstName, lastName, nationalCode, tel, email, field, university, certificate, enterDate, creditCardNumber, credit);
     }
 }
