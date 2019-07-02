f7d0b7adcc95181a092a3ef84a3175e5593219e4
ali <aliamiryance@gmail.com>
2019-07-02 11:17:06 +0430

    edit managements
    Signed-off-by: ali <aliamiryance@gmail.com>

--- src/service/Search.java
+++ src/service/Search.java
@@ -1,25 +1,32 @@
 package service;
 
+import DB.Db_Handler;
 import Model.Document;
 import Model.User;
+import setting.Setting;
 
 import java.util.List;
 
 public class Search {
 
-    public static List<User> searchUser(){
-        return null;
+    public static List<User> searchUser(String input){
+        List<User> users =  Db_Handler.getDatabaseHandler(Setting.Db_Table_name.User).findUser(input);
+        return users;
     }
 
-    public static List<Document> searchDocument(){
-        return null;
+    public static List<Document> searchDocument(String input){
+        List<Document> documents = Db_Handler.getDatabaseHandler(Setting.Db_Table_name.Document).findDocument(input);
+        return documents;
     }
 
-    public static List<User> filterUser(){
-        return null;
+    public static List<User> filterUser(String... atr){
+        List<User> users = Db_Handler.getDatabaseHandler(Setting.Db_Table_name.User).findUser(atr);
+        return users;
     }
 
-    public static List<Document> filterDocument(){
-        return null;
+    public static List<Document> filterDocument(String... atr){
+        List<Document> documents = Db_Handler.getDatabaseHandler(Setting.Db_Table_name.Document).findDocument(atr);
+        return documents;
     }
+    
 }
