29d7fc95836344297c51bc99988f692b03896290
maze <m_akbarizarkesh@yahoo.com>
2019-07-02 12:23:42 +0430

    Send Message Add to project

--- src/Model/Order.java
+++ src/Model/Order.java
@@ -5,4 +5,44 @@ public class Order {//todo need to complete!
     private Date orderDate;
     private Document document;
     private User orderUser;
+    private boolean isSuccessFull;
+
+    public Order(Date orderDate, Document document, User orderUser, boolean isSuccessFull) {
+        this.orderDate = orderDate;
+        this.document = document;
+        this.orderUser = orderUser;
+        this.isSuccessFull = isSuccessFull;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Document getDocument() {
+        return document;
+    }
+
+    public void setDocument(Document document) {
+        this.document = document;
+    }
+
+    public User getOrderUser() {
+        return orderUser;
+    }
+
+    public void setOrderUser(User orderUser) {
+        this.orderUser = orderUser;
+    }
+
+    public boolean isSuccessFull() {
+        return isSuccessFull;
+    }
+
+    public void setSuccessFull(boolean successFull) {
+        isSuccessFull = successFull;
+    }
 }
